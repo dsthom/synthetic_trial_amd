eb198efe2bb72ea11e220be38da4c084eb205aca
dsthom <darren_thomas@outlook.com>
2019-11-04 16:26:06 +0000

    add StudyExit

--- src/synthetic_arm_study_table.sql
+++ src/synthetic_arm_study_table.sql
@@ -1,38 +1,30 @@
 -- !preview conn=DBI::dbConnect(RMySQL::MySQL(), user = "admin", password = "password", dbname = "NOVA5", host = "127.0.0.1", port = 9999)
 -- Script to create & populate the study table: AMDSyntheticEyleaMeta having run synthetic_arm_large_tables.sql
-
+  
 -- create table to be used for this study only
 DROP TABLE IF EXISTS AMDSyntheticEyleaArmMeta;
-
+  
 CREATE TABLE AMDSyntheticEyleaArmMeta
   SELECT PatientID, Eye, SiteID, Gender
   FROM PatientMeta 
   WHERE (PatientID, SiteID, Eye) IN (SELECT PatientID, SiteID, EyeCode FROM nvAMDSurgeryIndications);
-
+  
 CREATE INDEX idx_AMDSyntheticEyleaArmMeta_pt_st_e ON AMDSyntheticEyleaArmMeta (`PatientID`, `SiteID`, `Eye`);
 CREATE INDEX idx_AMDSyntheticEyleaArmMeta_pt_st ON AMDSyntheticEyleaArmMeta (`PatientID`, `SiteID`);
-
+  
 -- extend table with columns we will later populate
 ALTER TABLE AMDSyntheticEyleaArmMeta
-  ADD COLUMN AgeAtBaseline INT(11) DEFAULT NULL,
   ADD COLUMN BaselineDate DATE DEFAULT NULL,
+  ADD COLUMN EstimatedStudyExit DATE DEFAULT NULL,
   ADD COLUMN StudyExit DATE DEFAULT NULL, 
   ADD COLUMN EyleaStartDate DATE DEFAULT NULL,
   ADD COLUMN AvastinStartDate DATE DEFAULT NULL,
   ADD COLUMN LucentisStartDate DATE DEFAULT NULL,
-  ADD COLUMN InjectionCount INT(11) DEFAULT NULL,
-  ADD COLUMN va_0 INT(11) DEFAULT NULL,
-  ADD COLUMN va_1 INT(11) DEFAULT NULL,
-  ADD COLUMN va_6 INT(11) DEFAULT NULL,
-  ADD COLUMN va_12 INT(11) DEFAULT NULL,
-  ADD COLUMN va_18 INT(11) DEFAULT NULL,
-  ADD COLUMN va_24 INT(11) DEFAULT NULL,
-  ADD COLUMN va_30 INT(11) DEFAULT NULL,
-  ADD COLUMN va_36 INT(11) DEFAULT NULL,
-  ADD COLUMN va_42 INT(11) DEFAULT NULL,
-  ADD COLUMN va_48 INT(11) DEFAULT NULL,
-  ADD COLUMN va_54 INT(11) DEFAULT NULL,
+  ADD COLUMN StudyEligible INT(11) DEFAULT NULL,
+  ADD COLUMN Bilateral INT(11) DEFAULT NULL,
   ADD COLUMN IndexEye INT(11) DEFAULT NULL,
+  ADD COLUMN AgeAtBaseline INT(11) DEFAULT NULL,
+  ADD COLUMN InjectionCount INT(11) DEFAULT NULL,
   ADD COLUMN SwitchExcl INT(11) DEFAULT NULL,
   ADD COLUMN RadioThermoExcl INT(11) DEFAULT NULL,
   ADD COLUMN VerteporfinThermoExcl INT(11) DEFAULT NULL,
@@ -44,7 +36,7 @@ ALTER TABLE AMDSyntheticEyleaArmMeta
   ADD COLUMN GlaucomaSurgExcl INT(11) DEFAULT NULL,
   ADD COLUMN CornealTransplantExcl INT(11) DEFAULT NULL,
   ADD COLUMN SubMacSurgExcl INT(11) DEFAULT NULL;
-
+  
 -- BaselineDate (date of first Eylea injection)
 UPDATE AMDSyntheticEyleaArmMeta p
 SET p.BaselineDate = (
@@ -53,17 +45,37 @@ FROM nvAMDInjections i
 WHERE p.PatientID = i.PatientID AND p.Eye = i.EyeCode AND p.SiteID = i.SiteID
 AND i.InjectedDrugDesc = 'Eylea 2 mg/0.05ml (aflibercept)'
 );
+  
+-- EstimatedStudyExit (the date 378 days (54 weeks * 7 days) onwards from baseline)
+UPDATE AMDSyntheticEyleaArmMeta
+SET EstimatedStudyExit = DATE_ADD(BaselineDate, INTERVAL 378 DAY);
+
+-- StudyExit (date of va measurement closest---but prior---to EstimatedStudyExit)
+UPDATE AMDSyntheticEyleaArmMeta p
+SET StudyExit = (
+  SELECT v.Date
+  FROM nvAMDVisualAcuity v
+  WHERE p.PatientID = v.PatientID AND 
+        p.Eye = v.Eye AND 
+        p.SiteID = v.SiteID AND
+        v.Date > p.BaselineDate AND
+        v.Date <= p.EstimatedStudyExit
+  ORDER BY SQRT(DATEDIFF(v.Date, p.EstimatedStudyExit) ^ 2)
+  LIMIT 1
+);
 
 -- EyleaStartDate
 UPDATE AMDSyntheticEyleaArmMeta p
 SET p.EyleaStartDate = (
-SELECT i.EncounterDate
+SELECT MIN(i.EncounterDate)
 FROM nvAMDInjections i
 WHERE p.PatientID = i.PatientID AND p.Eye = i.EyeCode AND p.SiteID = i.SiteID AND i.AntiVEGFInjection = 1
 AND InjectedDrugDesc = "Eylea 2 mg/0.05ml (aflibercept)"
-ORDER BY i.EncounterDate 
-LIMIT 1
 );
+
+-- Delete all eyes not admninistered Eylea
+DELETE FROM AMDSyntheticEyleaArmMeta 
+WHERE EyleaStartDate IS NULL;
 
 -- AvastinStartDate
 UPDATE AMDSyntheticEyleaArmMeta p
@@ -87,115 +99,41 @@ ORDER BY i.EncounterDate
 LIMIT 1
 );
 
--- va_0 (va taken closest to baseline, up to a maxium of n days before 14 days before)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_0 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) + 14 BETWEEN 0 AND 14
-);
+-- StudyEligible (1 if Eylea is first VEGF inhibitor administered)
+UPDATE AMDSyntheticEyleaArmMeta
+SET StudyEligible = CASE 
+                     WHEN EyleaStartDate IS NOT NULL AND
+                          EyleaStartDate < AvastinStartDate OR
+                          EyleaStartDate < LucentisStartDate
+                     THEN 1
+                     ELSE 0
+                     END;
 
--- va_1 (va at 1 to 7 days after baseline) (is this viable or ignore)?
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_1 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 1 AND 7
-);
+-- Bilateral (1 if > 1 eye in table)
 
--- va_6 (va at 35 to 42 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_6 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 35 AND 42
-);
+================================
+UPDATE AMDSyntheticEyleaArmMeta
+SET Bilateral = 1
 
--- va_12 (va at 77 to 83 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_12 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 77 AND 83
-);
+SELECT PatientID, COUNT(PatientID)
+FROM AMDSyntheticEyleaArmMeta
+WHERE 
+GROUP BY PatientID;
+================================
 
--- va_18 (va at 119 to 125 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_18 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 119 AND 125
-);
+-- IndexEye (if > 1 eye affected, assign the eye diagnosed earliest THEN>>>>>)
+================================
 
--- va_24 (va at 161 to 167 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_24 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 161 AND 167
-);
+================================
 
--- va_30 (va at 203 to 209 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_30 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 203 AND 209
-);
 
--- va_36 (va at 245 to 251 days after baseline)
+-- AgeAtBaseline (years between perturbed date of bith and BaselineDate)
 UPDATE AMDSyntheticEyleaArmMeta p
-SET va_36 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 245 AND 251
-);
+JOIN ETCPatientDetails d
+ON p.PatientID = d.PatientID 
+SET AgeAtBaseline = DATEDIFF(BaslineDate, PerturbedDateofBirth) / 365.25;
 
--- va_42 (va at 287 to 293 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_42 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 287 AND 293
-);
-
--- va_48 (va at 329 to 335 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_48 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 329 AND 335
-);
-
--- va_54 (va at 371 to 377 days after baseline)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET va_54 = (
-  SELECT MAX(v.ETDRS)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 371 AND 377
-);
-
--- StudyExit (date as the date of week 54 va measurement)
-UPDATE AMDSyntheticEyleaArmMeta p
-SET StudyExit = (
-  SELECT MIN(v.Date)
-  FROM nvAMDVisualAcuity v
-  WHERE p.PatientID = v.PatientID AND p.Eye = v.Eye AND p.SiteID = v.SiteID AND
-  (DATEDIFF(v.Date, p.BaselineDate)) BETWEEN 371 AND 377 AND p.va_54 = v.ETDRS
-);
-
--- InjectionCount (number injections reieved during study period)
+-- InjectionCount (number of injections reieved during study period)
 UPDATE AMDSyntheticEyleaArmMeta p
 SET InjectionCount = (
   SELECT COUNT(DISTINCT i.EncounterDate)
@@ -204,8 +142,6 @@ SET InjectionCount = (
   i.EncounterDate >= BaselineDate AND 
   i.EncounterDate <= StudyExit
 );
-
--- IndexEye ()
 
 -- SwitchExcl (1 if AvastinStartDate OR LucentisStartDate <= StudyExit)
 UPDATE AMDSyntheticEyleaArmMeta
@@ -237,18 +173,23 @@ SET RadioThermoExcl = 0
 WHERE RadioThermoExcl IS NULL;
 
 -- VerteporfinThermoExcl (PDR (as a proxy of verteporfin) within 7 days of baseline in non-study eye)
+================================
 
+================================
 
 UPDATE AMDSyntheticEyleaArmMeta
 SET VerteporfinThermoExcl = 0
 WHERE VerteporfinThermoExcl IS NULL;
 
 -- ClinicalTrialExcl (Macugen, Avastin or Luncetis before baseline---irregardless of whether during clinical trial)
+================================
 
+================================
 
 UPDATE AMDSyntheticEyleaArmMeta
 SET ClinicalTrialExcl = 0
 WHERE ClinicalTrialExcl IS NULL;
+
 -- IntravitrealExcl (intravitreal corticosteroid injection or implanttation before baseline)
 UPDATE AMDSyntheticEyleaArmMeta p
 JOIN ETCInjections i
@@ -365,13 +306,16 @@ SET CornealTransplantExcl = 0
 WHERE CornealTransplantExcl IS NULL;
 
 -- SubMacSurgExcl
+# Any surgery for indications associated with AMD phenotype
 
 
 UPDATE AMDSyntheticEyleaArmMeta
 SET SubMacSurgExcl = 0
 WHERE SubMacSurgExcl IS NULL;
 
+================================
 -- Update all Excl fills that are NULL to 0 (this is where there were no joins—or perhaps an anti-join update query for each?)
+================================
 
 -- Export to .csv (& convert variable names to snake case)
 SELECT  
@@ -382,17 +326,7 @@ SELECT
    AvastinStartDate AS avastin_start_date,
    LucentisStartDate AS lucentis_start_date,
    InjectionCount AS injection_count,
-   va_0,
-   va_1, 
-   va_6,
-   va_12, 
-   va_18,
-   va_24, 
-   va_30,
-   va_36,
-   va_42,
-   va_48,
-   va_54,
+   StudyElligible,
    IndexEye AS index_eye,
    SwitchExcl AS switch_excl,
    RadioThermoExcl AS radio_thermo_excl,
