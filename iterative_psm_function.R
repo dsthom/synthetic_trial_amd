2bec0d1aea61bef32e6fbacb5a6573214d104574
dsthom <darren_thomas@outlook.com>
2020-02-06 17:11:54 +0000

    + bug fix.

--- iterative_psm_function.R
+++ iterative_psm_function.R
@@ -1,30 +1,32 @@
 # function for iterative psm
+# input is list of propensity scores
 
 library(tidyverse)
 
-iterative_psm <- function(df, match_ratio, caliper_sd) {
-  
-  set.seed(1337)
-  
-  for (i in (df)){
-    ps <- df %>% 
-      sample_n(size = nrow(df),
-               replace = FALSE)
-    
-    psm <- Matching::Match(Tr = ps$treatment[ps$match_counter == "0"], 
-                           X = ps$propensity_score [ps$match_counter == "0"],
-                           M = match_ratio, 
-                           caliper = caliper_sd,
-                           replace = FALSE,
-                           ties = FALSE)
-    
-    matches <- ps[unlist(psm[c("index.treated", "index.control")]), ]
-    
-# update match_counter if matched
-  }
-  
-  matches <- matches %>% 
-    nest(iteration)
-}
+output <- list()
 
-# input is list of ps X matches with a variatible itration (1 to 10)
+iterative_psm <- function(list, match_ratio, caliper_sd, baseline_seed) {
+  
+  set.seed(baseline_seed)
+  
+  for (i in seq_along(list)){
+    # filter for unmatched
+    iteration.list[[i]] <- iteration.list[[i]] %>% 
+      filter(match_counter == 0) %>% 
+    # shuffle order
+      sample_n(size = nrow(ps), # n must be equal to n_rows
+               replace = FALSE)  
+    # match on propensity score
+    output[[i]] <- Matching::Match(Tr = iteration.list[[i]]$treatment,
+                                   X = iteration.list[[i]]$propensity_score,
+                                   M = match_ratio, 
+                                   caliper = caliper_sd,
+                                   replace = FALSE,
+                                   ties = FALSE)
+    # update match_counter if matched
+    
+    # set new seed
+    set.seed(baseline_seed + i)
+  }
+  output
+}
