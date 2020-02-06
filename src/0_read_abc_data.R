f9080d1b563ff3a03b22c9449e0a7a58fc625fb5
dsthom <darren_thomas@outlook.com>
2020-02-06 17:02:55 +0000

    + tidy, long_form

--- src/0_read_abc_data.R
+++ src/0_read_abc_data.R
@@ -1,3 +1,5 @@
+# script to transform original .csv files
+
 library(tidyverse)
 
 # ------------------------------------------------------------------------------
@@ -5,62 +7,68 @@ library(tidyverse)
   
 abc.acuity <- read_csv("data/abc_trial_acuity.csv",
                 col_types = cols(
-                  studynumber = col_character(),
+                  studynumber = col_integer(),
                   lost = col_factor(),
                   arm = col_factor(),
                   treatment = col_factor(),
                   trt = col_factor()
                 ))
 
-# Shorten variable names that start_with "vast"
 abc.acuity  <- abc.acuity  %>% 
-  rename_at(vars(starts_with("vast")), funs(str_replace(., "vast", "va_")))
+  # shorten variable names that start_with "vast"
+  rename_at(vars(starts_with("vast")), funs(str_replace(., "vast", ""))) %>% 
+  # convert treatment strings to lowercase 
+  mutate(treatment = tolower(treatment)) %>% 
+  # convert dataframe to long-form
+  pivot_longer(cols = 2:12,
+               names_to = "week",
+               values_to = "etdrs") %>% 
+  # arrange by studynumber & then week
+  arrange(studynumber, week) %>% 
+  # rename studynumber to snake_case 
+  rename(study_number = studynumber)
 
-# Reorder va by chronology
-abc.acuity  <- abc.acuity  %>% 
-  select(abc_id = studynumber,
-         lost,
-         arm,
-         treatment,
-         trt,
-         va_0,
-         va_1,
-         va_6,
-         va_12,
-         va_18,
-         va_24,
-         va_30,
-         va_36,
-         va_42,
-         va_48,
-         va_54)
-            
-# convert treatment strings to lower_case
-abc.acuity  <- abc.acuity  %>% 
-  mutate(treatment = tolower(treatment))
-  
-# restrict to those randomised to recieve avastin
-abc.acuity  <- abc.acuity  %>% 
-  filter(treatment == "avastin")
+write_csv(abc.acuity, "data/abc_va_longform.csv")
 
-# tidy data using pivot_longer() or gather()
-#. Perhaps unnecessary
+# ------------------------------------------------------------------------------
+# baseline visual acuity
+
+baseline.acuity <- abc.acuity %>% 
+  filter(week == 0)
 
 # ------------------------------------------------------------------------------
 # age
+
 abc.age <- read_csv("data/abc_trial_age.csv",
                     col_types = cols(
-                      studyNumber = col_character()
+                      studyNumber = col_integer()
                     ))
 
 abc.age <- abc.age %>% 
-  select(abc_id = studyNumber,
-         age = Age,
-         avastin_n = avastintrt)
+  # rename variables to lowercase
+  rename(study_number = studyNumber,
+         age_at_baseline = Age,
+         injection_count = avastintrt) %>% 
+  # convert Gender strings to M & F in parity with ehr data
+  mutate(gender = case_when(gender == "Male" ~ "M",
+                            gender == "Female" ~ "F")) %>% 
+  # convert treatment strings to lowercase 
+  mutate(treatment = tolower(treatment)) %>% 
+  # append baseline va
+  left_join(select(baseline.acuity, 
+                   study_number, 
+                   baseline_etdrs = etdrs),
+            by = "study_number",
+            keep = FALSE) %>% 
+  # transform study_number to character
+  mutate(study_number = as.character(study_number)) %>% 
+  # keep only variables relevant to PS
+  select(id = study_number,
+         treatment,
+         injection_count,
+         gender,
+         age_at_baseline,
+         baseline_etdrs)
 
-
-# merge dataframes
-abc.arm <- abc.acuity %>% 
-  left_join(abc.age,
-            by = "abc_id",
-            keep = FALSE)
+write_csv(abc.age, "data/abc_patient_characteristics.csv")
+ 
