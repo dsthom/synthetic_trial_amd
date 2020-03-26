82a06863c4fba8ad8e843e72a4523fb4aed4f818
dsthom <darren_thomas@outlook.com>
2020-03-26 19:10:37 +0000

    initial commit.

---
+++ src/kale_mysql.R
@@ -0,0 +1,11 @@
+# script to connet to kale mysql server
+
+library(keyring)
+
+mysql <- DBI::dbConnect(RMySQL::MySQL(),
+                        user = key_list("kale_mysql")[1, 2], 
+                        password = key_get("kale_mysql", 
+                                           key_list("kale_mysql")[1, 2]),
+                        dbname = "NOVA6", 
+                        host = "127.0.0.1",
+                        port = 9999)
