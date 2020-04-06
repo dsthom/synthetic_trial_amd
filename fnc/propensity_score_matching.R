e069b319d3f06d6af37fb882274936135e4d4cde
dsthom <darren_thomas@outlook.com>
2020-04-06 22:26:17 +0100

    checkpoint before splitting to list_iterations()

--- fnc/propensity_score_matching.R
+++ fnc/propensity_score_matching.R
@@ -2,9 +2,7 @@
 # input: abc (8 by 65) & ehr (8 by 4,472)
 # output: 
 
-# score abc Avastin arm
-
-propensity_score_matching <- function(abc, ehr, iterations, caliper) {
+propensity_score_matching <- function(trial, synthetic, iterations, caliper) {
   
   # set seed for reproducible sampling
   set.seed(1337)
@@ -12,28 +10,67 @@ propensity_score_matching <- function(abc, ehr, iterations, caliper) {
   # source propensity model
   source("src/propensity_model.R")
   
-  # predict Pr of treatment(Avastin == 1)
-  abc.psm <- augment(x = propensity_model,
-                           newdata = abc,
-                           type.predict = "response") %>% 
+  # predict Pr of treatment(Avastin == 1) for trial arm
+  trial.psm <- augment(
+    x = propensity_model,
+    newdata = trial,
+    type.predict = "response") %>% 
     # rename .fitted and .se.fit
     rename(
       propensity_score = .fitted,
       propensity_score_se = .se.fit
     )
   
-  # create a list of dataframes equal to iterations
-  abc.psm <- rep(list(abc.psm), iterations)
+  # predict Pr of treatment(Avastin == 1) for synthetic arm
+  synthetic.psm <- augment(
+    x = propensity_model,
+    newdata = synthetic,
+    type.predict = "response") %>% 
+    # rename .fitted and .se.fit
+    rename(
+      propensity_score = .fitted,
+      propensity_score_se = .se.fit
+    )  %>% 
+    # create a match_counter that is upated to 1 if matched
+    mutate(match_counter = 0)
   
-  for(i in seq_along(abc.psm)) {
-    abc.psm[[i]][["iteration"]] <-  i
+  # create a list of trial dataframes equal to iterations
+  trial.list <- rep(list(trial.psm), iterations)
+  
+  for(i in seq_along(trial.list)) {
+    trial.list[[i]][["iteration"]] <-  i
   }
   
-  abc.psm <- do.call(rbind.data.frame, abc.psm)
+  trial.list <- do.call(rbind.data.frame, trial.list)
   
-  abc.psm <- abc.psm %>% 
+  # convert to list-column
+  trial.list <- trial.list %>% 
     group_by(iteration) %>% 
-    nest()
+    nest() %>% 
+    mutate(data = map(
+      .x = data,
+      ~ sample_n(
+        .x,
+        size = nrow(.x),
+        repalce = FALSE
+      )
+    ))
   
-  # shuffle order 
+  # in-exact matching
+  
+  ## calculate propensity_score width of 0.1 standard deviation
+  caliper_ps <- trial.psm %>% 
+    filter(treatment == 'avastin') %>% 
+    summarise(caliper = sd(propensity_score))   ###!!! multiply by 0.1/caliper argument variable
+    
+  trial.list <- trial.list %>% 
+    mutate(matches = map(
+      .x = data,
+      ~ fuzzyjoin::difference_left_join(
+        x = .x,
+        y = synthetic.psm,
+        by = "propensity_score",
+        max_dist = caliper_ps
+      )
+    ))
 }
