12364b46f8b7f5b6c41fdc799795acee8fe99b86
dsthom <darren_thomas@outlook.com>
2020-06-02 20:56:36 +0100

    — remove iteration arugment. + encode caliper_ps as vector

--- fnc/propensity_score_matching.R
+++ fnc/propensity_score_matching.R
@@ -3,9 +3,8 @@
 # output: 
 
 propensity_score_matching <- function(
-  trial_arm, 
-  synthetic_arm, 
-  iterations = 1, # shortcut without depricating argument until fix
+  trial_arm, # table of
+  synthetic_arm, # table of
   caliper) {
   
   # set seed for reproducible sampling
@@ -34,47 +33,88 @@ propensity_score_matching <- function(
     rename(
       propensity_score = .fitted,
       propensity_score_se = .se.fit
-    )  %>% 
-    # create a match_counter that is upated to 1 if matched
-    mutate(match_counter = 0)
+    )
   
-  # create a list of trial dataframes equal to iterations
-  trial.list <- rep(list(trial.psm), iterations)
-  
-  for(i in seq_along(trial.list)) {
-    trial.list[[i]][["iteration"]] <-  i
-  }
-  
-  trial.list <- do.call(rbind.data.frame, trial.list)
-  
-  # convert to list-column
-  trial.list <- trial.list %>% 
-    group_by(iteration) %>% 
+  # calculate propensity_score width of 0.1 standard deviation
+
+  caliper_ps <- sd(trial.psm$propensity_score[trial.psm$treatment == 'avastin'])
+  caliper_ps <- caliper_ps * caliper
+
+  # inexact matching
+  ps.matches <- trial.psm %>% 
+    # convert to list-column
+    group_by(id) %>% 
     nest() %>% 
-    mutate(data = map(
-      .x = data,
-      ~ sample_n(
-        .x,
-        size = nrow(.x),
-        replace = FALSE
-      )
-    ))
-  
-  # in-exact matching
-  
-  ## calculate propensity_score width of 0.1 standard deviation
-  caliper_ps <- trial.psm %>% 
-    filter(treatment == 'avastin') %>% 
-    summarise(caliper = sd(propensity_score))   ###!!! multiply by 0.1/caliper argument variable
-    
-  trial.list <- trial.list %>% 
+    # tibble of all exact ps matches
     mutate(matches = map(
       .x = data,
       ~ fuzzyjoin::difference_left_join(
         x = .x,
         y = synthetic.psm,
         by = "propensity_score",
-        max_dist = caliper_ps
+        max_dist = caliper_ps 
       )
-    ))
+    )) %>% 
+    # randomly sample 1 match for each trial eye
+    mutate(sampled_match = map(
+      .x = matches,
+      ~ sample_n(
+        .x,
+        size = 1
+      )
+    )) %>% 
+    # unnest list column
+    select(sampled_match) %>% 
+    unnest() %>% 
+    ungroup() %>% 
+    # rename ids
+    rename(
+      abc_id = id,
+      ehr_id = id1
+    ) %>% 
+    # drop rows that have 0 matches
+    drop_na(ehr_id)
+  
+  # create longform list of matched eyes for future filtering export a list in longform of matches and pair_id
+  matched.pairs <- ps.matches %>% 
+    select(
+      abc = abc_id,
+      ehr = ehr_id) %>% 
+    mutate(pair_id = 1:nrow(ps.matches)) %>% 
+    pivot_longer(
+      cols = abc:ehr,
+      names_to = "cohort",
+      values_to = "id")
+  
+  # create output table ammenable for logistic regression
+  # trial
+  matched.trial <- trial.psm %>% 
+    # filter for only eyes that were matched
+    filter(id %in% matched.pairs$id) %>% 
+    # add pair_id
+    left_join(select(matched.pairs, id, pair_id),
+              by = "id")
+  
+  # synthetic
+  matched.synthetic <- synthetic.psm %>% 
+    # filter for only eyes that were matched
+    filter(id %in% matched.pairs$id) %>% 
+    # add pair_id 
+    left_join(select(matched.pairs, id, pair_id),
+              by = "id")
+  
+  # combine tables into one
+  output <- bind_rows(matched.trial, matched.synthetic) %>% 
+    # rearrange row order
+    select(id,
+           treatment, 
+           pair_id,
+           propensity_score,
+           age,
+           gender,
+           baseline_etdrs,
+           drug_load,
+           drug_recency,
+           study_exit_va)
+  
 }
