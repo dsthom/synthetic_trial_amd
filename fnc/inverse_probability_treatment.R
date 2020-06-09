7f03c78f80b40b027494574eb6754e053e531c0c
dsthom <darren_thomas@outlook.com>
2020-06-09 16:15:26 +0100

    + add ps_trimming argument

--- fnc/inverse_probability_treatment.R
+++ fnc/inverse_probability_treatment.R
@@ -5,7 +5,8 @@
 #
 inverse_probability_treatment <- function(
   trial_arm, 
-  synthetic_arm){
+  synthetic_arm,
+  ps_trimming){
   
   # source propensity model
   source("src/propensity_model.R")
@@ -14,9 +15,22 @@ inverse_probability_treatment <- function(
   target.trial <- bind_rows(trial_arm, synthetic_arm)
   
   # predict Pr of treatment(Avastin == 1)
-  iptw <- augment(x = propensity_model,
+  output <- augment(x = propensity_model,
                   newdata = target.trial,
                   type.predict = "response") %>% 
     # calculate inverse probability of treatment weights
-    mutate(ipw = 1 / .fitted)  
+    mutate(ipw = 1 / .fitted)
+  
+  if(ps_trimming == TRUE){
+    output <- output %>% 
+      filter(between(
+        ipw,
+        min(output$ipw[output$treatment == 'avastin']),
+        max(output$ipw[output$treatment == 'avastin'])
+      ))
+  }
+  
+  output
+  
 }
+
